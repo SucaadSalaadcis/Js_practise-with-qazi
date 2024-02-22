f58983c2c3705f139349d014c691f86697298b6a
sucaad salaad <SucaadSalaad520@gmail.com>
2024-02-21 20:55:49 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("suad salad osman");
+
+/*The prompt() is a built-in javaScript fuctionality
+that helps you get inputs from a user through the browser
+*/
+fruit = prompt ('What is your favorite fruit');
+console.log(fruit);
+food = 20;
+tipPrcentage = 0.2;
+tipAmount = food * tipPrcentage;
+console.log(tipAmount);
+
+// user input
+
